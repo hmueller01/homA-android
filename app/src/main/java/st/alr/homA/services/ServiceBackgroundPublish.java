fa67bc9a7a7a19836d2e9616cd0bb12b8720ce11
hmueller01 <CuOckUpayW1d>
2017-04-14 22:15:50 +0200

    code cleanup, fixed some Android Studio warnings, moved from depreciated onStart() to onStartCommand()

--- app/src/main/java/st/alr/homA/services/ServiceBackgroundPublish.java
+++ app/src/main/java/st/alr/homA/services/ServiceBackgroundPublish.java
@@ -1,10 +1,6 @@
 
 package st.alr.homA.services;
-import java.util.ArrayList;
 
-import st.alr.homA.model.Quickpublish;
-import st.alr.homA.support.Defaults;
-import st.alr.homA.support.Events;
 import android.app.Service;
 import android.content.Intent;
 import android.nfc.NdefMessage;
@@ -14,31 +10,30 @@ import android.nfc.Tag;
 import android.nfc.tech.Ndef;
 import android.os.IBinder;
 import android.util.Log;
-import de.greenrobot.event.EventBus;
 
-public class ServiceBackgroundPublish extends Service
-{
+import java.util.ArrayList;
+
+import de.greenrobot.event.EventBus;
+import st.alr.homA.model.Quickpublish;
+import st.alr.homA.support.Defaults;
+import st.alr.homA.support.Events;
+
+public class ServiceBackgroundPublish extends Service {
     private boolean waitingForConnection = false;
     Runnable deferred;
 
     @Override
-    public void onCreate()
-    {
+    public void onCreate() {
         super.onCreate();
-
     }
 
     @Override
-    public void onStart(final Intent intent, final int startId)
-    {
-        
+    public int onStartCommand(Intent intent, int flags, int startId) {
         Log.v(this.toString(), "Background publish service started ");
-        //EventBus.getDefault().register(this, Events.StateChanged.ServiceMqtt.class);
         EventBus.getDefault().register(this);
         startService(new Intent(this, ServiceMqtt.class));
-        
-        if (intent != null) {
 
+        if (intent != null) {
             String action = intent.getAction();
 
             if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
@@ -60,18 +55,18 @@ public class ServiceBackgroundPublish extends Service
                         }
                     }
                 }
-
-            } else if (action.equals("st.alr.homA.action.QUICKPUBLISH")){
+            } else if (action.equals("st.alr.homA.action.QUICKPUBLISH")) {
                 Log.v(this.toString(), "Background notification publish");
                 handleMessage(intent.getStringExtra("qp"));
             } else {
-                Log.d(this.toString(), "Unknown intent with action: " + action);   
+                Log.d(this.toString(), "Unknown intent with action: " + action);
             }
         } else {
             Log.v(this.toString(), "NFC Service started without intent, abandon ship");
         }
 
         stopSelf();
+        return super.onStartCommand(intent, flags, startId);
     }
 
     private void handleMessage(String quickpublishJson) {
@@ -81,7 +76,6 @@ public class ServiceBackgroundPublish extends Service
             Log.v(this.toString(), "Got Quickpublish from tag: " + qp.toString());
             ServiceMqtt.getInstance().publish(qp.getTopic(), qp.getPayload(), qp.isRetained(), 0, 20, null, null);
         }
-
     }
 
     public void onEvent(Events.StateChanged.ServiceMqtt event) {
